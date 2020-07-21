bf6c948b0982eadf5bd357221561bba136e2ac48
unknown <ninnavarroalejandro@gmail.com>
2020-07-21 16:58:48 -0400

    Added a new style for the calculator

--- CSS Desígn/Calculator/main.js
+++ CSS Desígn/Calculator/main.js
@@ -1,1 +1,43 @@
-var button9 = document.getElementById('b9');
+class Calculator{
+
+    constructor(previousOperationText, currentOperationText){
+
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+    }
+
+    clear() {
+
+
+    }
+
+    delete() {
+
+
+    }
+
+    appendNumber(number) {
+
+
+    }
+
+    chooseOperation(operation){
+
+
+    }
+
+    compute(){
+
+
+    }
+
+    
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalButtons = document.querySelector('[data-equal]');
+const deleteButtons = document.querySelector('[data-delete]');
+const allClearButtons = document.querySelector('[data-all-clear]');
+const currentOperationText = document.querySelector('[data-current-operantion]');
+const previousOperationText = document.querySelector('[data-previous-operation]');
